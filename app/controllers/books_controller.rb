9c432a519554af7fe652030e51159b245bd3ccb7
yuta <calendula6724yuta@gmail.com>
2024-02-17 08:59:24 +0900

    herokuデプロイテスト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,16 @@
 class BooksController < ApplicationController
+
+def search
+    if params[:query].nil?
+      return
+    elsif params[:query].blank?
+      flash.now[:danger] = '検索キーワードが入力されていません'
+    else
+      url = "https://www.googleapis.com/books/v1/volumes"
+      text = params[:query]
+      res = Faraday.get(url, q: text, langRestrict: 'ja', maxResults: 20)
+      @books = JSON.parse(res.body)
+    end
+  end
+
 end
