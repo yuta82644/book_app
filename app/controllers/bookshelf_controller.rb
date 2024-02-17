5d0f3bf616899fe261909613bfdb0a739181dc39
yuta <calendula6724yuta@gmail.com>
2024-02-17 14:42:36 +0900

    CRUD機能実装

--- app/controllers/bookshelf_controller.rb
+++ app/controllers/bookshelf_controller.rb
@@ -1,4 +1,3 @@
-
 class BookshelfController < ApplicationController
   def index
     @books = Book.all
@@ -8,7 +7,31 @@ class BookshelfController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  
+  def add_review
+    @book = Book.find(params[:id])
+    @book.update(review: params[:review])
+    redirect_to bookshelf_path(@book), notice: '感想を書きました'
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to bookshelf_path, notice: '感想を更新しました'
+    else
+      render :edit, alert: '感想の更新に失敗しました'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to bookshelf_index_path, notice: '本を削除しました'
+  end
+
   private
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'search', to: 'books#search'
   post 'add_to_bookshelf', to: 'books#add_to_bookshelf'
   resources :bookshelf do
-
+    member do
+      post 'add_review'
+    end
   end
 end
