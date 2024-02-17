24ffdeacf5e494279248baee5971d2e984e04d5c
yuta <calendula6724yuta@gmail.com>
2024-02-17 11:50:57 +0900

    show画面実装

--- app/controllers/bookshelf_controller.rb
+++ app/controllers/bookshelf_controller.rb
@@ -1,5 +1,17 @@
+
 class BookshelfController < ApplicationController
   def index
     @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :published_date, :image_url, :review)
   end
 end
